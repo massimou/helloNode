5314e5e556a7210dab9d9030be52c120f6ced96e
massimou <39625296+massimou@users.noreply.github.com>
2018-05-25 16:20:52 +0200

    Add files via upload

---
+++ nodejs/hello.js
@@ -0,0 +1,5 @@
+console.log('Node is installed!');
+const name = "Andrew";
+const details = { favouriteLanguage: "JavaScript", age: 33, children: 3 } 
+const errorMessage = "Something bad has occurred";
+console.log(name);
